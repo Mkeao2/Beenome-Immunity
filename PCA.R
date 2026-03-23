b86c7ee85d1b06c1b441e55d8de27d9d1149556c
MKeao2 <Makayleekcrone@gmail.com>
2026-03-23 14:39:34 -0400

    From R

--- PCA.R
+++ PCA.R
@@ -1,45 +1,48 @@
 
 library(tidyverse)
+library(dplyr)
+library(readr)
+library(vegan)
 
-overlap_mat <- read.table("Data Files/Orthogroups_SpeciesOverlaps.tsv",
-                      header = TRUE,
-                      row.names = 1,
-                      sep = "\t",
-                      check.names = FALSE)
 
 # Read orthogroup totals
 og_counts <- read.csv("Data Files/orthogroup_counts.csv")
-
+orthogroups <- read_tsv("Data Files/Orthogroups.tsv")
 
 family_map <- read.csv("Data Files/Trait Data/Families.csv")
 diet_map <- read.csv("Data Files/Trait Data/Diet.csv")
 nesting_map <- read.csv("Data Files/Trait Data/Nesting_Substrate.csv")
-sociality_map <- read.csv("Data Files/Trait Data/Sociality.csv")
+sociality_map <- read.csv("Data Files/Trait Data/Sociality_Sarah.csv")
 
-# ----------------------------
-# NORMALIZATION STEP
-# ----------------------------
-# Scale overlap values to 0–1 with Jaccard distances
+# -------------------------------------------
+# Option to filter to only include immune orthogroups
+# -------------------------------------------
 
-# Make named vector
-og_vec <- og_counts$Total_Orthogroups
-names(og_vec) <- og_counts$Species
+immune_og <- read_csv("Data Files/Immune_Orthogroup_Annotation_Current.csv")
 
-# Ensure species order matches matrix
-og_vec <- og_vec[rownames(overlap_mat)]
+immune_ids <- immune_og$Orthogroup
 
-# Normalization by total number of orthogroups per species
-min_mat <- outer(og_vec, og_vec, pmin)
-norm_mat2 <- overlap_mat / min_mat
+# FOR IMMUNE ANALYSIS ONLY! Filter Orthogroups file
+immune_orthogroups <- orthogroups %>%
+  filter(Orthogroup %in% immune_ids)
 
-overlap_mat <- norm_mat2
+# -------------------------------------------
+# Transform counts to presence/absence matrix
+# -------------------------------------------
+
+presence_mat <- orthogroups
+
+presence_mat[,-1] <- ifelse(is.na(presence_mat[,-1]), 0, 1)
+
+presence_mat <- as.matrix(presence_mat[,-1])
 
 #remove outliers and ref species
 overlap_mat <- overlap_mat[!rownames(overlap_mat) %in% c("Drosophila_melanogaster", "Anopheles_gambiae"), ]
 
-# Convert similarity to distance
-dist_mat <- 1 - norm_mat2
-dist_obj <- as.dist(dist_mat)
+#Jaccard distance
+dist_obj <- vegdist(t(presence_mat), method = "jaccard")
+
+t(presence_mat)
 
 
 # ----------------------------
@@ -57,48 +60,56 @@ scores2 <- left_join(scores, family_map, by = "Species")
 #removes rows with missing values
 scores2 <- scores2 %>% filter(!is.na(Family))
 
+string_to_remove <- "Semisocial" 
+scores2 <- scores2 %>% filter(!grepl(string_to_remove, Sociality))
+
 # Variance explained
 var_exp <- round(pcoa$eig / sum(pcoa$eig) * 100, 1)
 
 # ----------------------------
-# Plot
-# ----------------------------
-ggplot(scores2, aes(PCoA1, PCoA2, color = Family, label = Species)) +
-  geom_point(size = 2) +
-  #geom_text(vjust = -0.6, size = 3) +
-  theme_bw() +
-  labs(
-    title = "PCoA of Gene Overlap by Family",
-    x = paste0("PCoA1 (", var_exp[1], "%)"),
-    y = paste0("PCoA2 (", var_exp[2], "%)"),
-    color = "Diet"
-  )
-
-#Zoom in on clusters
-ggplot(scores2, aes(PCoA1, PCoA2, color = Family)) +
-  geom_point(size = 2) +
-  coord_cartesian(xlim = c(-0.03, 0.04), ylim = c(-0.06, 0.04)) +
-  theme_bw() +
-  labs(
-    title = "PCoA of Gene Overlap by Family",
-    x = paste0("PCoA1 (", var_exp[1], "%)"),
-    y = paste0("PCoA2 (", var_exp[2], "%)"),
-    color = "Family"
-  )
-
-#This method tells us how similar species are based on the number of genes they share
-
-
-# ----------------------------
 # PERMANOVA
 # ----------------------------
-
-library(vegan)
 # Distance matrix in ordination space
 ord_dist <- dist(scores2[, c("PCoA1", "PCoA2")])
 
 # PERMANOVA
-adonis_result <- adonis2(ord_dist ~ Family, data = scores2, permutations = 999)
+adonis2(ord_dist ~ Family, data = scores2, permutations = 999)
 
-adonis_result
+# ----------------------------
+# Plot
+# ----------------------------
+  
+ggplot(scores2, aes(PCoA1, PCoA2, color = Family, label = Family)) +
+  geom_point(size = 2) +
+  #geom_text(vjust = -0.6, size = 3) +
+  theme_bw() +
+  labs(
+    title = "PCoA of Gene Overlap by Family - All Orthogroups",
+    x = paste0("PCoA1 (", var_exp[1], "%)"),
+    y = paste0("PCoA2 (", var_exp[2], "%)"),
+    color = "Sociality"
+  ) + 
+  annotate("text", x = 0.05, y = 0.05,
+           label = "R^2 == 0.02", 
+           parse = TRUE) +
+  annotate("text", x = 0.05, y = 0.037,
+           label = "P == 0.56", 
+           parse = TRUE)
+           hjust = 0.1, vjust = 1)
+
+
+#Zoom in on clusters
+ggplot(scores2, aes(PCoA1, PCoA2, color = Family)) +
+  geom_point(size = 2) +
+  coord_cartesian(xlim = c(-0.02, 0.01), ylim = c(-0.05, 0.04)) +
+  theme_bw() +
+  labs(
+    title = "PCoA of Gene Overlap by Sociality",
+    x = paste0("PCoA1 (", var_exp[1], "%)"),
+    y = paste0("PCoA2 (", var_exp[2], "%)"),
+    color = "Sociality"
+  ) 
+
+
+
 
